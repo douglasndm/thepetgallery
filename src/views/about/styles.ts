88b6ed5a0a7e7b43e5247ea491352101f86e7488
Douglas <7841509+douglasndm@users.noreply.github.com>
2025-10-11 04:28:21 -0300

    fix: more header position

--- src/views/about/styles.ts
+++ src/views/about/styles.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.ScrollView.attrs(() => ({
-	contentInsetAdjustmentBehavior: 'automatic',
 	alwaysBounceVertical: true,
 	contentContainerStyle: {
 		flexGrow: 1,
