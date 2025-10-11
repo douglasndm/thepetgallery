0c5b79bf7cecf2782f26e3df7884fcee428ac3f6
Douglas <7841509+douglasndm@users.noreply.github.com>
2025-10-11 04:44:03 -0300

    adding eas project

--- configs/expo/extra.ts
+++ configs/expo/extra.ts
@@ -3,6 +3,9 @@ interface extra {
 }
 
 const configs: extra = {
+	eas: {
+		projectId: '3ce8ca86-09ca-4432-9f90-0468e97b1684',
+	},
 	API_URL_DOG: process.env.API_URL_DOG ?? '',
 	API_URL_CAT: process.env.API_URL_CAT ?? '',
 
