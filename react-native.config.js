051eb8929c1a254fa8d642589c18521f86929fdf
Douglas de Mattos <douglasnunesdemattos@outlook.com>
2026-04-08 09:54:44 -0300

    removing react-native-sfsymbols

--- react-native.config.js
+++ react-native.config.js
@@ -1,10 +1,5 @@
 module.exports = {
 	dependencies: {
-		'react-native-sfsymbols': {
-			platforms: {
-				android: null, // Ignora no Android
-			},
-		},
 		'@react-native-community/blur': {
 			platforms: {
 				android: null, // Ignora no Android
