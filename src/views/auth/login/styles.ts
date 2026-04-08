2b45d82d368b20c371a681cd6f7a29eb602c04f1
Douglas de Mattos <douglasnunesdemattos@outlook.com>
2026-04-08 01:42:18 -0300

    fix: login without scrollview

--- src/views/auth/login/styles.ts
+++ src/views/auth/login/styles.ts
@@ -1,7 +1,13 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
-	flex: 1;
+export const Container = styled.ScrollView.attrs(() => ({
+	contentInsetAdjustmentBehavior: 'never',
+	alwaysBounceVertical: true,
+	contentContainerStyle: {
+		flexGrow: 1,
+		paddingBottom: 32,
+	},
+}))`
 	background-color: #e8e8ea;
 `;
 
